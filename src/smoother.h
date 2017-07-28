9f2de9433dfa7cc210522ceea0c25e0036d14ad6
Jeremy Shannon <jeremyeshannon@gmail.com>
2017-07-28 15:34:05 -0500

    add smoother.h

---
+++ src/smoother.h
@@ -0,0 +1,26 @@
+#ifndef SMOOTHER
+#define SMOOTHER
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include "spline.h"
+
+using namespace std;
+
+vector<vector<double>> interpolate_points(vector<double> x_vals, vector<double> y_vals) {
+
+  // uses the spline library to interpolate points connecting a series of x and y values
+
+  vector< vector<double> > output;
+
+  if (x_vals.size() != y_vals.size()) {
+    cout << "ERROR! SMOOTHER: interpolate_points size mismatch between x_vals and y_vals" << endl;
+    return{ { 0 }, { 0 } };
+  }
+}
+
+
+
+#endif
