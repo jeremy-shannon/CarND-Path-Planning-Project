080361cf448602501386da4d5467f2cb7f19c270
Jeremy Shannon <jeremyeshannon@gmail.com>
2017-08-05 16:45:50 -0500

    clean up

--- src/jmt.cpp
+++ src/jmt.cpp
@@ -11,30 +11,22 @@ using Eigen::VectorXd;
 
 using namespace std;
 
-vector<double> get_traj_coeffs(vector< double> start, vector <double> end, double T)
+vector<double> get_traj_coeffs(vector<double> start, vector<double> end, double T)
 {
     /*
     Calculate the Jerk Minimizing Trajectory that connects the initial state
     to the final state in time T.
 
     INPUTS
-
     start - the vehicles start location given as a length three array
         corresponding to initial values of [s, s_dot, s_double_dot]
-
     end   - the desired end state for vehicle. Like "start" this is a
         length three array.
-
     T     - The duration, in seconds, over which this maneuver should occur.
 
     OUTPUT 
     an array of length 6, each value corresponding to a coefficent in the polynomial 
     s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
-
-    EXAMPLE
-
-    > JMT( [0, 10, 0], [10, 10, 0], 1)
-    [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
     */
 
     MatrixXd a(3,3);
