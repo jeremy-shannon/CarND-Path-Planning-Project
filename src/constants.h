a63a7fcc4c079bd2ad3ec3606592dc68a18cc6dc
Jeremy Shannon <jeremyeshannon@gmail.com>
2017-08-05 16:19:38 -0500

    add weight constants

--- src/constants.h
+++ src/constants.h
@@ -10,8 +10,8 @@
 #define N_SAMPLES = 50
 #define DT = 0.2                        // seconds
 
-#define SIGMA_S = [10.0, 4.0, 2.0]      // s, s_dot, s_double_dot
-#define SIGMA_D = [1.0, 1.0, 1.0]
+#define SIGMA_S = {10.0, 4.0, 2.0}      // s, s_dot, s_double_dot
+#define SIGMA_D = {1.0, 1.0, 1.0}
 #define SIGMA_T = 2.0
 
 #define MAX_INSTANTANEOUS_JERK = 10     // m/s/s/s
@@ -22,5 +22,17 @@
 
 #define SPEED_LIMIT = 22                // m/s
 
+// cost function weights
+
+#define TIME_DIFF_COST_WEIGHT 10
+#define TRAJ_DIFF_COST_WEIGHT 10
+#define COLLISION_COST_WEIGHT 9999
+#define BUFFER_COST_WEIGHT 10
+#define SPEED_LIMIT_COST_WEIGHT 1000
+#define EFFICIENCY_COST_WEIGHT 100
+#define MAX_ACCEL_COST_WEIGHT 1000
+#define AVG_ACCEL_COST_WEIGHT 100
+#define MAX_JERK_COST_WEIGHT 1000
+#define AVG_JERK_COST_WEIGHT 100
 
 #endif
