28b4f78598f6c2ddcdcfb3c68391ebc204eb172a
Jeremy Shannon <jeremyeshannon@gmail.com>
2017-08-10 11:57:41 -0400

    add and edit some of the constants

--- src/constants.h
+++ src/constants.h
@@ -2,33 +2,36 @@
 #define CONSTANTS
 
 #define VEHICLE_RADIUS 1.5              // meters
+#define FOLLOW_DISTANCE 6.0             // distance to keep behind leading cars
+#define PERCENT_V_DIFF_TO_MAKE_UP 0.5   // the percent difference between current velocity and target velocity to allow ego car to make up in a single trajectory      
 
 #define PREVIOUS_PATH_POINTS_TO_KEEP 10
 #define NEW_PATH_POINTS_TO_ADD 40
 
 #define TRACK_LENGTH 6945.554           // meters
 
-#define N_SAMPLES = 150
-#define DT = 0.02                       // seconds
+#define N_SAMPLES 150
+#define DT 0.02                         // seconds
 
-#define NUM_RANDOM_TRAJ_TO_GEN          // the number of perturbed trajectories to generate (for each duration)
+#define NUM_RANDOM_TRAJ_TO_GEN 10       // the number of perturbed trajectories to generate (for each duration)
+#define NUM_TIMESTEPS_TO_PERTURB 5      // the number of timesteps, +/- target time, to perturb trajectories
 
 // sigma values for perturbing targets
-#define SIGMA_S = 10.0                  // s
-#define SIGMA_S_DOT = 4.0               // s_dot
-#define SIGMA_S_DDOT = 2.0              // s
-#define SIGMA_D = 1.0                   // d
-#define SIGMA_D_DOT = 1.0               // d_dot
-#define SIGMA_D_DDOT = 1.0              // d_double_dot
-#define SIGMA_T = 2.0
+#define SIGMA_S 10.0                    // s
+#define SIGMA_S_DOT 4.0                 // s_dot
+#define SIGMA_S_DDOT 2.0                // s
+#define SIGMA_D 1.0                     // d
+#define SIGMA_D_DOT 1.0                 // d_dot
+#define SIGMA_D_DDOT 1.0                // d_double_dot
+#define SIGMA_T 2.0
 
-#define MAX_INSTANTANEOUS_JERK = 10     // m/s/s/s
-#define MAX_INSTANTANEOUS_ACCEL= 10     // m/s/s
+#define MAX_INSTANTANEOUS_JERK 10       // m/s/s/s
+#define MAX_INSTANTANEOUS_ACCEL 10      // m/s/s
 
-#define EXPECTED_JERK_IN_ONE_SEC = 2    // m/s/s
-#define EXPECTED_ACC_IN_ONE_SEC = 1     // m/s
+#define EXPECTED_JERK_IN_ONE_SEC 2      // m/s/s
+#define EXPECTED_ACC_IN_ONE_SEC 1       // m/s
 
-#define SPEED_LIMIT = 22                // m/s
+#define SPEED_LIMIT 22                  // m/s
 
 // cost function weights
 #define TIME_DIFF_COST_WEIGHT 10
