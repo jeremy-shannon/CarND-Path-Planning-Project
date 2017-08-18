710a4413cbd8fbe14d4a9b13d566a53759cb8cf3
Jeremy Shannon <jeremyeshannon@gmail.com>
2017-08-18 10:50:32 -0500

    add overloaded interpolated points, exchanging fixed size/interval for set of evaluation points

--- src/smoother.h
+++ src/smoother.h
@@ -12,7 +12,7 @@ using namespace std;
 vector<double> interpolate_points(vector<double> pts_x, vector<double> pts_y, 
                                   double interval, int output_size) {
   // uses the spline library to interpolate points connecting a series of x and y values
-  // output is output_size number of y values beginning at y[0] with specified interval
+  // output is output_size number of y values beginning at y[0] with specified fixed interval
 
   if (pts_x.size() != pts_y.size()) {
     cout << "ERROR! SMOOTHER: interpolate_points size mismatch between pts_x and pts_y" << endl;
@@ -24,6 +24,25 @@ vector<double> interpolate_points(vector<double> pts_x, vector<double> pts_y,
   vector<double> output;
   for (int i = 0; i < output_size; i++) {
     output.push_back(s(pts_x[0] + i * interval));
+  }
+  return output;
+}
+
+vector<double> interpolate_points(vector<double> pts_x, vector<double> pts_y, 
+                                  vector<double> eval_at_x) {
+  // uses the spline library to interpolate points connecting a series of x and y values
+  // output is spline evaluated at each eval_at_x point
+
+  if (pts_x.size() != pts_y.size()) {
+    cout << "ERROR! SMOOTHER: interpolate_points size mismatch between pts_x and pts_y" << endl;
+    return { 0 };
+  }
+
+  tk::spline s;
+  s.set_points(pts_x,pts_y);    // currently it is required that X is already sorted
+  vector<double> output;
+  for (double x: eval_at_x) {
+    output.push_back(s(x));
   }
   return output;
 }
